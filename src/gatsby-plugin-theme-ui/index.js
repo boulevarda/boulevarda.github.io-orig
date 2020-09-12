8ed6ce5cdc433f2efa2550cf70f73c1143691f68
Tauren Mills <tauren@orbslabs.com>
2020-09-11 22:50:43 -0700

    updates

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    background: "ghostwhite",
+    text: "black",
+    primary: "mediumvioletred",
+    modes: {
+      dark: {
+        background: "indigo",
+        text: "ghostwhite",
+        primary: "gold",
+      },
+    },
+  }
+}
